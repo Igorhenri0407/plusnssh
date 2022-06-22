dfa00de300a80860f241e6192ad61aa6a3846918
igor <posseidon1989@gmail.com>
2022-06-22 06:36:30 -0300

    Obrigado Pai Seta Branca

--- gestorssh/instpainel.sh
+++ gestorssh/instpainel.sh
@@ -15,22 +15,22 @@ apt-get install figlet -y > /dev/null 2>&1
 rm /bin/pweb > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/pweb > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/pweb > /dev/null 2>&1
 chmod 777 pweb > /dev/null 2>&1
 clear
 mkdir /bin/ppweb > /dev/null 2>&1
 cd /bin/ppweb || exit
 rm *.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/install.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/ubuinst.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/senharoot.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/restbanco.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/restbanco18.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/empresa.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/updatepainel.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/updatepainel18.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/updatepainelarm.sh > /dev/null 2>&1
-wget https://github.com/Igorhenri0407/plusnssh/ntech/gestorssh/updatepainelarm18.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/install.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/ubuinst.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/senharoot.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/restbanco.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/restbanco18.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/empresa.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/updatepainel.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/updatepainel18.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/updatepainelarm.sh > /dev/null 2>&1
+wget https://github.com/Igorhenri0407/plusnssh/master/gestorssh/updatepainelarm18.sh > /dev/null 2>&1
 chmod 777 *.sh > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
